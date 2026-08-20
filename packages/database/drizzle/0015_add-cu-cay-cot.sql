296380ab6ba3fe6632c1fbd0006c2a8ab372f5a1
mrtee <tungfint@gmail.com>
2026-08-20 16:12:17 +0700

    feat: add Cu Cay Cot mascot

---
+++ packages/database/drizzle/0015_add-cu-cay-cot.sql
@@ -0,0 +1,16 @@
+INSERT INTO "rewards" (
+  "id", "name", "description", "cost", "stock", "active", "image_url",
+  "cash_value_vnd", "category", "required_cc_level", "requires_approval"
+) VALUES (
+  'c0de0007-0000-4000-8000-000000000007',
+  'Cú Cầy Cốt',
+  'Chú cú công nghệ thông thái, đồng hành cùng bạn quan sát test case, săn bug và chinh phục những bài toán khó.',
+  420,
+  NULL,
+  true,
+  '/mascots/cu-cay-cot.webp',
+  NULL,
+  'MASCOT',
+  1500,
+  false
+);
